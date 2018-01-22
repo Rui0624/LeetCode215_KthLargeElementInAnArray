b9476bc496327793403413f202b613cce731ef90
Rui <rui@dhcp88.merusouth120.iit.edu>
2018-01-21 23:33:00 -0600

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,9 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class Solution {
+	public int findKthLargest(int[] nums, int k) {
+       Arrays.sort(nums);
+       return nums[nums.length - k];
+    }
+}
